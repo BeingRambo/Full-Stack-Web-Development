dabadef8b6e8d3ff34998e32e9da6cd577984173
Sanjay Gawai <112196917+BeingRambo@users.noreply.github.com>
2023-03-28 02:42:36 +0530

    Add files via upload

--- Serving all Html FIles/index.js
+++ Serving all Html FIles/index.js
@@ -1,1 +1,84 @@
-adad
+// html module
+
+const html = require("http");
+const port = 8000;
+const fs = require('fs')   // file system module
+
+
+
+
+
+//request handler
+function requesthandler(req, res){
+    console.log(res.url);
+    res.writeHead(200, {"Content-type": "text/html"})
+
+
+//                   //path              //error  //file data 
+//    fs.readFile("./index.html", function(err, data){
+      
+//     if(err){
+//         console.log("erroe", err);
+//         return res.end("<h1>Error</h1>");
+//     }
+//     return res.end(data);
+    
+
+//    });
+
+
+let filepath;
+
+switch(req.url){
+    case "/":
+        filepath ="./index.html"
+        break;
+    case "/profile":
+        filepath ="./profile.html"
+        break;
+    
+    default:
+        filepath = "./404.html"    
+        
+}
+
+fs.readFile(filepath, function(err, data){
+
+    if(err){
+        console.log("error", err);
+        return res.end("<h1>Error</h1>");
+    }
+
+    return res.end(data);
+})
+}
+
+
+
+ 
+
+
+//creating server
+
+const server = html.createServer(requesthandler);
+
+
+
+//server listner
+server.listen(port, function(err){   
+    if(err)
+    {
+        console.log(err);
+        return;
+    }
+                                  
+    console.log("Server is running on port:",port);
+});
+
+
+
+
+
+//installing nodemon  type         npm install nodemon -g
+// checking nodemon                nodemon -v
+//accessing files                  nodemon index.js
