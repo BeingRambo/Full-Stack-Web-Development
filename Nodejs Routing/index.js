477e57f25d55200693a70f14d831c60a7d41844c
Sanjay Gawai <112196917+BeingRambo@users.noreply.github.com>
2023-03-27 23:46:57 +0530

    Add files via upload

--- Nodejs Routing/index.js
+++ Nodejs Routing/index.js
@@ -1,1 +1,52 @@
-//handling routing
+//http module
+const http = require('http');
+const port = 8000;
+
+
+
+
+
+
+
+// create server
+
+const server = http.createServer((req, res)=>{
+      
+    //url module for req   abouts home contact us
+     if(req.url == "/"){
+        res.end("Home");      
+     }
+     else if(req.url == "/about")
+     {
+        res.end("About Us");   
+     }
+
+     else if(req.url == "/contact")
+     {
+        res.end("Contact Us");   
+     }
+
+     else if(req.url == "/services")
+     {
+        res.end("Services");   
+     }
+     else{
+        res.writeHead(404, {"Content-type": "text/html" })
+        res.end("<h1> 404 error pages. page doesn't exist </h1>")
+     }
+
+
+
+
+                      //response
+
+});
+
+
+
+//listneing req
+
+server.listen(port, "127.0.0.1",()=>{                                  // taking two paramerters (port, localhost)
+
+    console.log("listening to the port:",port)
+});
