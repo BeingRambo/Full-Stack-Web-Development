bdb4f93b6237d679bf067bca544bc7d310b3b4b6
Sanjay Gawai <112196917+BeingRambo@users.noreply.github.com>
2023-03-28 20:19:53 +0530

    Add files via upload

--- StartingExpress/contact_list/index.js
+++ StartingExpress/contact_list/index.js
@@ -1,1 +1,27 @@
-dadad
+const express = require("express")
+const port = 8000;
+
+const app = express();
+
+//returning resonse
+//According to the API reference, the first param always contain request req, then response res.
+
+
+app.get('/', function(req, res){    
+    res.send("<h1>Successfully run</h1>"); // automatically changing content-type
+});
+
+
+
+
+
+
+
+app.listen(port, function(err){
+
+    if(err)
+    {
+        console.log("error")
+    }
+    console.log("express server running on",port)
+})
